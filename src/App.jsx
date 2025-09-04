b398cf5a347e877be9ea29df2d5c1820c63929a5
RohitBiradar <rohitpramilabiradar@gmail.com>
2025-09-04 17:19:54 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,18 @@ import { Provider } from "react-redux";
 import store from "store/store.js";
 import { CartProvider } from "pages/Cart/CartContext.jsx";
 import { WishlistProvider } from "pages/Wishlist/WishlistContext.jsx";
-import { ClerkProvider } from "@clerk/clerk-react";
-import conf from "conf/conf.js";
 
 function App() {
   return (
-    <ClerkProvider publishableKey={conf.clerkPublishableKey}>
-      <BrowserRouter>
-        <Provider store={store}>
-          <WishlistProvider>
-            <CartProvider>
-              <ProjectRoutes />
-            </CartProvider>
-          </WishlistProvider>
-        </Provider>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <WishlistProvider>
+          <CartProvider>
+            <ProjectRoutes />
+          </CartProvider>
+        </WishlistProvider>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import conf from "conf/conf.js";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <ClerkProvider publishableKey={conf.clerkPublishableKey}>
+    <App />
+  </ClerkProvider>
+);
