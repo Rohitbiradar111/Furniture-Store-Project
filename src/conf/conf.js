947d01139fadec8de2bf696d8af3c7ecf0c30668
RohitBiradar <rohitpramilabiradar@gmail.com>
2025-09-04 17:51:17 +0530

    made some changes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-  clerkPublishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
-  emailjsServiceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  emailjsTemplateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-  emailjsUserId: import.meta.env.VITE_EMAILJS_USER_ID,
-  razorpayKeyId: import.meta.env.VITE_RAZORPAY_KEY_ID,
+  clerkPublishableKey: String(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY),
+  emailjsServiceId: String(import.meta.env.VITE_EMAILJS_SERVICE_ID),
+  emailjsTemplateId: String(import.meta.env.VITE_EMAILJS_TEMPLATE_ID),
+  emailjsUserId: String(import.meta.env.VITE_EMAILJS_USER_ID),
+  razorpayKeyId: String(import.meta.env.VITE_RAZORPAY_KEY_ID),
 };
 
 export default conf;
